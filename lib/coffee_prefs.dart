ad085394bdff5669ee1f29d0e33f5c77c651f0cd
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-04-05 17:02:19 +0200

    feat: FilledButton

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class CoffeePrefs extends StatelessWidget {
   const CoffeePrefs({super.key});
 
+  void increaseStrength() {
+    print('inc strength by 1');
+  }
+
+  void increaseSugars() {
+    print('inc sugars by 1');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,7 +26,14 @@ class CoffeePrefs extends StatelessWidget {
               colorBlendMode: BlendMode.multiply,
             ),
             Expanded(child: const SizedBox()),
-            const Text('+'),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseStrength,
+              child: const Text('+'),
+            ),
           ],
         ),
         Row(
@@ -32,7 +47,14 @@ class CoffeePrefs extends StatelessWidget {
               colorBlendMode: BlendMode.multiply,
             ),
             Expanded(child: const SizedBox()),
-            const Text('+'),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseSugars,
+              child: const Text('+'),
+            ),
           ],
         ),
       ],
