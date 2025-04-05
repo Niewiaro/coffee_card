b3f849a677130b7de21d0034cb805a3848dad293
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-04-05 16:12:49 +0200

    chore: Sandbox Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
-import 'package:coffee_card/sandbox/sandbox_column.dart';
+// import 'package:coffee_card/sandbox/sandbox_column.dart';
+import 'package:coffee_card/sandbox/sandbox_row.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: SandboxColumn()
+    home: Sandbox()
   ));
 }
